68b1875540d6504b84a9ccba70363269d4a9b80e
mohamedbenrabah <86411808+mohamedbenrabah@users.noreply.github.com>
2022-02-18 16:43:58 +0100

    user version1

---
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -0,0 +1,39 @@
+package tn.esprit.spring.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@ToString
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long userId;
+	String login;
+	String password;
+	Integer registrationNumber; 
+	String FirstName;
+	String LastName;
+	Date BirthDate;
+	String socialStatus;
+	String profilePicture;
+	
+}
