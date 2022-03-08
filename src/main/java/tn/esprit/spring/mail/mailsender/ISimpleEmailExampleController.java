b38ac7431c4d0e01be86f86e8f51fac7712f5e54
issamunic <89040136+issamunic@users.noreply.github.com>
2022-03-08 16:11:02 +0100

    Revert "5dmet houssem"
    This reverts commit 4664d982dbafde1232c160c3b7fc63a63bffd3ee, reversing
    changes made to 8befa1dcb3487b50a84f7e95b6b2ae22e5461ddb.

--- src/main/java/tn/esprit/spring/mail/mailsender/ISimpleEmailExampleController.java
+++
@@ -1,6 +0,0 @@
-package tn.esprit.spring.mail.mailsender;
-
-public interface ISimpleEmailExampleController {
-
-	public String sendSimpleEmail(String Mail , String Subject , String Text);
-}
--- src/main/java/tn/esprit/spring/springbatch/csv/csvRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.spring.springbatch.csv;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import tn.esprit.spring.entities.csv;
-
-public interface csvRepository extends JpaRepository<csv, Integer>{
-
-}
