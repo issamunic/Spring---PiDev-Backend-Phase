fa1d58724b6e148056125fe5ea23aed38526c880
issamunic <89040136+issamunic@users.noreply.github.com>
2022-03-10 10:17:59 +0100

    a

---
+++ src/main/java/tn/esprit/spring/repository/MatchedListRepository.java
@@ -0,0 +1,22 @@
+package tn.esprit.spring.repository;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import tn.esprit.spring.entities.MatchedList;
+
+
+
+@Repository
+public interface MatchedListRepository extends JpaRepository<MatchedList, Long> {
+	
+}
+	
+	
+	
